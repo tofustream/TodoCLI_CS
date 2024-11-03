1a8b6e6285934cdb787cee888a424d0937d3b11d
tofustream <177463621+tofustream@users.noreply.github.com>
2024-11-04 00:54:26 +0900

    titleに関するvoを作成

---
+++ TodoCLI_CS/Domain/VOs/Title.cs
@@ -0,0 +1,29 @@
+﻿namespace TodoCLI_CS.Domain.VOs
+{
+    internal class Title
+    {
+        private readonly string _value;
+
+        public Title(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("title must not be empty");
+            }
+
+            if (value.Length > 100)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value), "please keep the title under 100 characters");
+            }
+
+            _value = value;
+        }
+
+        public string Value => _value;
+
+        public override string ToString()
+        {
+            return _value;
+        }
+    }
+}
