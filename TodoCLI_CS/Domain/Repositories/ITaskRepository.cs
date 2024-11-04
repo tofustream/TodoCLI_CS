fdbd944930fef36e3a4309e0a2957beb6ae7488a
tofustream <177463621+tofustream@users.noreply.github.com>
2024-11-05 00:32:47 +0900

    TaskRepositoryのInterfaceを実装。 DTOではなく、エンティティを返す仕様とする。

---
+++ TodoCLI_CS/Domain/Repositories/ITaskRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace TodoCLI_CS.Domain.Repositories
+{
+    public interface ITaskRepository
+    {
+        // 新しいタスクを追加する
+        void Add(Entities.Task task);
+
+        // IDによってタスクを取得する
+        Entities.Task GetById(Guid id);
+
+        // タスクを更新する
+        void Update(Entities.Task task);
+
+        // タスクを削除する
+        void Delete(Guid id);
+
+        // すべてのタスクを取得する
+        IEnumerable<Entities.Task> GetAllTasks();
+    }
+}
