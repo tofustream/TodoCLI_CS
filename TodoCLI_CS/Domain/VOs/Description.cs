2b31f43acc41a68ff5c5f65fe9a75e977ba44933
tofustream <177463621+tofustream@users.noreply.github.com>
2024-11-04 12:35:43 +0900

    値オブジェクトとしてDescriptionを作成

---
+++ TodoCLI_CS/Domain/VOs/Description.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoCLI_CS.Domain.VOs
+{
+    public class Description
+    {
+        private string _value;
+
+        public Description(string value)
+        {
+            if (value.Length > 500)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value), "please keep the description under 500 characters");
+            }
+
+            _value = value;
+        }
+        public string Value => _value;
+
+        public override string ToString()
+        {
+            return _value;
+        }
+    }
+}
---
+++ TodoCLI_CSTests.Test/DescriptionTests.cs
@@ -0,0 +1,57 @@
+﻿using TodoCLI_CS.Domain.VOs;
+
+namespace TodoCLI_CSTests.Test
+{
+    [TestClass]
+    public class DescriptionTests
+    {
+        [TestMethod]
+        public void Constructor_ValidDescription_CreatesDescription()
+        {
+            string validDescription = "This is a valid description";
+
+            var description = new Description(validDescription);
+
+            Assert.IsNotNull(description);
+            Assert.AreEqual(validDescription, description.Value);
+        }
+
+        [TestMethod]
+        public void Constructor_DescriptionTooLong_ThrowsArgumentOutOfRangeException()
+        {
+            string longDescription = new string('a', 501);
+
+            var exception = Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Description(longDescription));
+            Assert.AreEqual("value", exception.ParamName);
+            Assert.AreEqual("please keep the description under 500 characters (Parameter 'value')", exception.Message);
+        }
+
+        [TestMethod]
+        public void Value_ReturnsCorrectDescription()
+        {
+            // Arrange
+            string validDescription = "A valid description";
+            var description = new Description(validDescription);
+
+            // Act
+            string result = description.Value;
+
+            // Assert
+            Assert.AreEqual(validDescription, result);
+        }
+
+        [TestMethod]
+        public void ToString_ReturnsCorrectDescription()
+        {
+            // Arrange
+            string validDescription = "ToString Test Description";
+            var description = new Description(validDescription);
+
+            // Act
+            string result = description.ToString();
+
+            // Assert
+            Assert.AreEqual(validDescription, result);
+        }
+    }
+}
